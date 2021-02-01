78a036e6e26faa6092c6710657d0003bbcf58e63
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-02-01 17:09:25 -0500

    Added the functionality to fetch & display users dynamically.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
-// // List of users
-// fetch('https://reqres.in/api/users')
-// .then(response => response.json())
-// .then(data => console.log('Data : ', data))
-// .catch(error => console.log('Error while fetching user data : ', error));
+document.addEventListener('DOMContentLoaded', () => {
 
-// // Users list with page number
-// fetch('https://reqres.in/api/users?page=2')
-// .then(response => response.json())
-// .then(data => console.log('Data : ', data))
-// .catch(error => console.log('Error while fetching user data : ', error));
+    let sectionResults = document.querySelector('.section-results');
 
-// Find user by ID
-fetch('https://reqres.in/api/users/1')
-.then(response => response.json())
-.then(data => console.log('Data : ', data))
-.catch(error => console.log('Error while fetching user data : ', error));
+    async function apiHandler(apiUrl) {
+        let response = await fetch(apiUrl);
+        let data = await response.json();
+        return data;
+    }
+
+    let getUsers = apiHandler(`https://reqres.in/api/users`);
+
+    getUsers.then(users => users.data.map(user => {
+
+        let userContainer = document.createElement('div');
+        userContainer.className = 'user-avatar';
+
+        sectionResults.appendChild(userContainer);
+
+        let userAvatar = document.createElement('img');
+
+        userAvatar.src = user.avatar;
+        userAvatar.alt = 'User Avatar';
+
+        userContainer.appendChild(userAvatar);
+
+        let userName = document.createElement('h5');
+
+        userName.innerText = `${user.first_name} ${user.last_name}`;
+
+        userContainer.appendChild(userName);
+
+        let userEmail = document.createElement('h6');
+
+        userEmail.innerText = user.email;
+
+        userContainer.appendChild(userEmail);
+
+    }));
+
+});
