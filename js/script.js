7e537ae08aadbd3c1e85cb5a8367ddecadf7ce73
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-02-01 17:42:05 -0500

    Implemented the functionality of displaying product count & pagination dynamically.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let sectionResults = document.querySelector('.section-results');
+    let sectionPagination = document.querySelector('.section-pagination');
 
     async function apiHandler(apiUrl) {
         let response = await fetch(apiUrl);
@@ -10,32 +11,77 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let getUsers = apiHandler(`https://reqres.in/api/users`);
 
-    getUsers.then(users => users.data.map(user => {
+    getUsers.then(users => {
 
-        let userContainer = document.createElement('div');
-        userContainer.className = 'user-avatar';
+        console.log(users);
 
-        sectionResults.appendChild(userContainer);
+        let paginationText = document.createElement('p');
 
-        let userAvatar = document.createElement('img');
+        if (users.data.length === 0) {
+            paginationText.innerText = ``
+        } else {
+            paginationText.innerText = `Displaying 1 to ${users.data.length} of ${users.total} users.`
+        }
 
-        userAvatar.src = user.avatar;
-        userAvatar.alt = 'User Avatar';
+        sectionPagination.appendChild(paginationText);
 
-        userContainer.appendChild(userAvatar);
+        let paginationContainer = document.createElement('div');
+        paginationContainer.className = 'page-numbers';
 
-        let userName = document.createElement('h5');
+        sectionPagination.appendChild(paginationContainer);
 
-        userName.innerText = `${user.first_name} ${user.last_name}`;
+        let buttonPrevious = document.createElement('p');
+        buttonPrevious.innerText = 'Previous';
 
-        userContainer.appendChild(userName);
+        paginationContainer.appendChild(buttonPrevious);
 
-        let userEmail = document.createElement('h6');
 
-        userEmail.innerText = user.email;
 
-        userContainer.appendChild(userEmail);
+        for (let i = 1; i <= users.total_pages; i++) {
 
-    }));
+            let pageNumberContainer = document.createElement('div');
+            paginationContainer.appendChild(pageNumberContainer);
+
+            let pageNumber = document.createElement('p');
+            pageNumber.innerText = i;
+
+            pageNumberContainer.appendChild(pageNumber);
+        }
+
+
+
+        let buttonNext = document.createElement('p');
+        buttonNext.innerText = 'Next';
+
+        paginationContainer.appendChild(buttonNext);
+
+        users.data.map(user => {
+
+            let userContainer = document.createElement('div');
+            userContainer.className = 'user-avatar';
+
+            sectionResults.appendChild(userContainer);
+
+            let userAvatar = document.createElement('img');
+
+            userAvatar.src = user.avatar;
+            userAvatar.alt = 'User Avatar';
+
+            userContainer.appendChild(userAvatar);
+
+            let userName = document.createElement('h5');
+
+            userName.innerText = `${user.first_name} ${user.last_name}`;
+
+            userContainer.appendChild(userName);
+
+            let userEmail = document.createElement('h6');
+
+            userEmail.innerText = user.email;
+
+            userContainer.appendChild(userEmail);
+
+        })
+    });
 
 });
