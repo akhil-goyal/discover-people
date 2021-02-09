d1e5ebc63b55a930beefb4ac0ac3c24767b70db0
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-02-09 16:50:37 -0500

    URL issue testing

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
+//  Waiting for the document's content to be
+//  loaded before performing any operation.
 document.addEventListener('DOMContentLoaded', () => {
 
     let sectionResults = document.querySelector('.section-results');
     let sectionPagination = document.querySelector('.section-pagination');
 
+    // Using async-await to handle the asynchronous response.
+    // Setting apiUrl as parameter.
     async function apiHandler(apiUrl) {
 
         let response = await fetch(apiUrl);
         let data = await response.json();
 
+        // Returning the json response from function apiHandler.
         return data;
     }
 
+    // Using function expression to get data in variable getUsers.
     let getUsers = apiHandler(`https://reqres.in/api/users?page=1`);
 
+    // Resolving the JSON data.
     getUsers.then(users => {
 
-        console.log(users);
-
+        // Putting the userdata in an array userArray
         let userArray = users.data;
 
         let paginationText = document.createElement('p');
 
+        // Setting data for pagination. If there are zero users, then no
+        // text would be displayed, however, if there are users, then
+        // pagination text is being set dynamically.
         if (users.data.length === 0) {
             paginationText.innerText = ``
         } else {
@@ -29,14 +38,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
         sectionPagination.appendChild(paginationText);
 
+        // Invoking the function loopData & 
+        // setting userArray as an argument.
         loopData(userArray);
 
     });
 
+    // Function loopDara that is responsible for looping through the
+    // user data & displaying the results.
     function loopData(userArray) {
 
-        sectionResults.innerHTML = '';
-
+        // Mapping through the array of users data.
         userArray.map(user => {
 
             let userContainer = document.createElement('div');
@@ -54,7 +66,6 @@ document.addEventListener('DOMContentLoaded', () => {
             let userName = document.createElement('p');
 
             userName.innerText = `${user.first_name} ${user.last_name}`;
-            // userName.href = './pages/user-profile.html';
 
             userContainer.appendChild(userName);
 
@@ -70,8 +81,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
             userContainer.appendChild(buttonView);
 
+            // Setting URL parameter on the click of a button. An user
+            // id has been used to set the parameter such that the user
+            // details may be retrieved on the following page.
+            // buttonView.addEventListener('click', function () {
+            //     location.href = `${location.protocol}//${location.host}/pages/user-profile.html?id=${user.id}`;
+            // });
+
             buttonView.addEventListener('click', function () {
-                location.href = `${location.protocol}//${location.host}/pages/user-profile.html?id=${user.id}`;
+                location.href = `/pages/user-profile.html?id=${user.id}`;
             });
         });
 
--- pages/user-profile.js
+++ pages/user-profile.js
@@ -1,13 +1,22 @@
 let sectionInfo = document.querySelector('.user-info');
 
+// Converting the current page URL into a string.
 let queryString = window.location.search;
+
+// Getting the parameters out of the stringified URL.
 let urlParameters = new URLSearchParams(queryString);
+
+// Looking for the URL parameter with name "id".
 let userId = urlParameters.get('id');
 
+// Using fetch to call the API along with user ID received through
+// URL parameter from home page.
 fetch(`https://reqres.in/api/users/${userId}`)
     .then(response => response.json())
     .then(response => showData(response.data));
 
+// Function showData that is responsbile for displaying
+// the user data received through API's response.
 function showData(user) {
     
     let userImage = document.createElement('img');
